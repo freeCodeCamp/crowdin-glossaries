16847e18e5f8c9d2fbf56976b53dfd59ad1ba5c7
Nicholas Carrigan <nhcarrigan@gmail.com>
2021-11-09 14:39:58 -0800

    feat: enable live runs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { glossaryIds, testingOpts } from "./configs/glossaryOpts";
+import { clientOpts, glossaryIds, testingOpts } from "./configs/glossaryOpts";
 import { uploadGlossary } from "./uploadGlossary";
 import { logHandler } from "./utils/logHandler";
 
@@ -6,6 +6,18 @@ import { logHandler } from "./utils/logHandler";
   const fileName = process.argv[2];
 
   switch (fileName) {
+    case "client":
+      logHandler.log("debug", `Starting process for ${fileName}.csv.`);
+      await uploadGlossary("client", clientOpts, glossaryIds.client);
+      break;
+    case "curriculum":
+      logHandler.log("debug", `Starting process for ${fileName}.csv.`);
+      await uploadGlossary("curriculum", testingOpts, glossaryIds.curriculum);
+      break;
+    case "docs":
+      logHandler.log("debug", `Starting process for ${fileName}.csv.`);
+      await uploadGlossary("docs", testingOpts, glossaryIds.docs);
+      break;
     case "testing":
       logHandler.log("debug", `Starting process for ${fileName}.csv.`);
       await uploadGlossary("testing", testingOpts, glossaryIds.testing);
